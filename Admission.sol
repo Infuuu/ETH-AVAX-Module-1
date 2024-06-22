4e6229f55a308c0803d40db1e0e3df79b4f1d318
Infuuu <121173358+Infuuu@users.noreply.github.com>
2024-06-22 18:33:42 +0530

    Update Admission.sol

--- Admission.sol
+++ Admission.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract Admission {
-    
+
     function uniA(uint mathMarks, uint scienceMarks) public pure returns(string memory){
         require(mathMarks > 80 && scienceMarks > 90, "You are not eligible for admission in our University");
         return "You are eligible to take admission";
     }
 
     function uniB(uint mathMarks, uint scienceMarks) public pure returns(string memory){
-        if(mathMarks > 60 && scienceMarks > 60){
+        if(mathMarks <= 60 || scienceMarks <= 60){
             revert("You are not eligible for admission in our University");
         }
         return "You are eligible to take admission";
